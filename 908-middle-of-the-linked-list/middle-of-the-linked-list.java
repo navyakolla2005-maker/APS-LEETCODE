8db3d3fd861eaf5f44231398b2421db606fe713d
navyakolla2005-maker <navyakolla2005@gmail.com>
2026-02-25 21:17:32 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (53.56%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;      // move 1 step
+            fast = fast.next.next; // move 2 steps
+        }
+
+        return slow;
+    }
+}
